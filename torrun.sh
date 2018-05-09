9ce1b724c03ad29e093cdea2de8163cab5755f3a
LNDMN <39122694+LNDMN@users.noreply.github.com>
2018-05-09 14:42:42 +0300

    Create torrun.sh

---
+++ torrun.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+_trans_port="9040"
+_int_if="ppp0"
+
+iptables -t nat -A PREROUTING -i $_int_if -p udp --dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A PREROUTING -i $_int_if -p tcp --syn -j REDIRECT --to-ports $_trans_port
+
+/etc/init.d/tor restart
