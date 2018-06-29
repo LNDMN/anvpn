1b85769c2e8560535b87324b683e77fbc49bb144
LNDMN <39122694+LNDMN@users.noreply.github.com>
2018-06-29 16:02:49 +0300

    Update torrun.sh

--- torrun.sh
+++ torrun.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 _trans_port="9040"
-_int_if="ppp0"
+_int_if="ppp+"
 
 iptables -t nat -A PREROUTING -i $_int_if -p udp --dport 53 -j REDIRECT --to-ports 53
 iptables -t nat -A PREROUTING -i $_int_if -p tcp --syn -j REDIRECT --to-ports $_trans_port
 
 /etc/init.d/tor restart
+
